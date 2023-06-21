af9ff7ced9240864bf8c07af588c95394847379b
BavleyAdel <119805780+BavleyAdel@users.noreply.github.com>
2023-06-21 19:12:03 +0300

    Delete MSP WEBSITE directory

--- MSP WEBSITE/main.js
+++
@@ -1,6 +0,0 @@
-let toggleIcon = document.querySelector(".menu-bars");
-let navItems = document.querySelector(".nav-items");
-
-toggleIcon.addEventListener("click" , function (params) {
-    navItems.classList.toggle(".d-flex");
-})
